054971504a747ba0a53f3ef48284f211962c4955
ayayk <git@ayatk.com>
2017-10-14 17:48:55 +0900

    ベンチマークを開始しちゃうボタンを追加した

--- app/src/main/java/kies/prepass/speedtest/MainActivity.kt
+++ app/src/main/java/kies/prepass/speedtest/MainActivity.kt
@@ -3,21 +3,46 @@ package kies.prepass.speedtest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kies.prepass.speedtest.api.PrepassClient
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        start_benchmark.setOnClickListener {
+            runBenchmark()
+            stop_benchmark.visibility = View.VISIBLE
+            start_benchmark.visibility = View.GONE
+        }
+
+        stop_benchmark.setOnClickListener {
+            stopBenchmark()
+            stop_benchmark.visibility = View.GONE
+            start_benchmark.visibility = View.VISIBLE
+        }
+    }
+
+    private fun stopBenchmark() {
+        Log.d("SpeedTest:stop", "Cancel ベンチマーク")
+        compositeDisposable.clear()
+    }
+
+    private fun runBenchmark() {
+
         // Gsonのベンチマーク
         val gsonFactory = GsonConverterFactory.create()
         var gsonStart = 0L
-        PrepassClient.service(gsonFactory)
+        compositeDisposable.add(PrepassClient.service(gsonFactory)
                 .getShop()
                 .subscribeOn(Schedulers.io())
                 .doOnSubscribe { gsonStart = System.nanoTime() }
@@ -25,11 +50,12 @@ class MainActivity : AppCompatActivity() {
                         { d -> Log.d("SpeedTest:gson", "finish -> ${System.nanoTime() - gsonStart}nano sec\nitem size -> ${d.size}") },
                         { t -> Log.e("SpeedTest:gson", "error $t") }
                 )
+        )
 
         // Moshiのベンチマーク
         val moshiFactory = MoshiConverterFactory.create()
         var moshiStart = 0L
-        PrepassClient.service(moshiFactory)
+        compositeDisposable.add(PrepassClient.service(moshiFactory)
                 .getShop()
                 .subscribeOn(Schedulers.io())
                 .doOnSubscribe { moshiStart = System.nanoTime() }
@@ -37,5 +63,6 @@ class MainActivity : AppCompatActivity() {
                         { d -> Log.d("SpeedTest:moshi", "finish -> ${System.nanoTime() - moshiStart}nano sec\nitem size -> ${d.size}") },
                         { t -> Log.e("SpeedTest:moshi", "error $t") }
                 )
+        )
     }
 }

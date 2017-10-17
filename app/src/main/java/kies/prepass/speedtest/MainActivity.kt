9637bbb4474edcfbbfc0cc52baaf39db6e8294d7
ayatk <git@ayatk.com>
2017-10-17 16:32:08 +0900

    ListViewに実装し直した

--- app/src/main/java/kies/prepass/speedtest/MainActivity.kt
+++ app/src/main/java/kies/prepass/speedtest/MainActivity.kt
@@ -1,20 +1,27 @@
 package kies.prepass.speedtest
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kies.prepass.speedtest.api.PrepassClient
 import kies.prepass.speedtest.databinding.ActivityMainBinding
-import kies.prepass.speedtest.models.ParseTime
-import kotlinx.android.synthetic.main.activity_main.*
+import kies.prepass.speedtest.databinding.ItemResultBinding
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.text.NumberFormat
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,67 +31,99 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
+    private lateinit var adapter: ResultAdapter
+
+    val formatNumber = NumberFormat.getNumberInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        start_benchmark.setOnClickListener {
+        adapter = ResultAdapter(this)
+        binding.resultList.adapter = adapter
+
+        binding.startBenchmark.setOnClickListener {
             runBenchmark()
-            stop_benchmark.visibility = View.VISIBLE
-            start_benchmark.visibility = View.GONE
+            binding.stopBenchmark.visibility = View.VISIBLE
+            binding.startBenchmark.visibility = View.GONE
         }
 
-        stop_benchmark.setOnClickListener {
-            stopBenchmark()
-            stop_benchmark.visibility = View.GONE
-            start_benchmark.visibility = View.VISIBLE
-        }
+        binding.stopBenchmark.setOnClickListener { stopBenchmark() }
     }
 
     private fun stopBenchmark() {
         Log.d("SpeedTest:stop", "Cancel ベンチマーク")
+        binding.stopBenchmark.visibility = View.VISIBLE
+        binding.startBenchmark.visibility = View.GONE
         compositeDisposable.clear()
     }
 
     private fun runBenchmark() {
-        val formatNumber = NumberFormat.getNumberInstance()
-
-        // Gsonのベンチマーク
-        val gsonFactory = GsonConverterFactory.create()
-        val gsonParseTime = ParseTime()
-        var gsonStart = 0L
-        compositeDisposable.add(PrepassClient.service(gsonFactory)
-                .getShop()
+        Single.create<Int> { emitter -> emitter.onSuccess(0) }
                 .subscribeOn(Schedulers.io())
-                .doOnSubscribe { gsonStart = System.nanoTime() }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        { d ->
-                            gsonParseTime.time = System.nanoTime() - gsonStart
-                            Log.d("SpeedTest:gson", "finish -> ${gsonParseTime.time}nano sec\nitem size -> ${d.size}")
-                            binding.gsonTimeText.text = "${formatNumber.format(gsonParseTime.time)}ns"
-                        },
-                        { t -> Log.e("SpeedTest:gson", "error $t") }
-                )
-        )
-
-        // Moshiのベンチマーク
-        val moshiFactory = MoshiConverterFactory.create()
-        val moshiParseTime = ParseTime()
-        var moshiStart = 0L
-        compositeDisposable.add(PrepassClient.service(moshiFactory)
-                .getShop()
+                .flatMap { _ -> gsonBenchmark() }
+                .flatMap { res ->
+                    adapter.add(res)
+                    moshiBenchmark()
+                }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe { moshiStart = System.nanoTime() }
                 .subscribe(
-                        { d ->
-                            moshiParseTime.time = System.nanoTime() - moshiStart
-                            Log.d("SpeedTest:moshi", "finish -> ${moshiParseTime.time}nano sec\nitem size -> ${d.size}")
-                            binding.moshiTimeText.text = "${formatNumber.format(moshiParseTime.time)}ns"
+                        { res ->
+                            adapter.add(res)
+                            binding.stopBenchmark.visibility = View.GONE
+                            binding.startBenchmark.visibility = View.VISIBLE
                         },
-                        { t -> Log.e("SpeedTest:moshi", "error $t") }
+                        { e -> Log.e("aa", e.toString()) }
                 )
-        )
+    }
+
+    private fun gsonBenchmark(): Single<Result> {
+        return Single.fromCallable {
+            var t0 = System.nanoTime()
+            PrepassClient.service(GsonConverterFactory.create())
+                    .getShop()
+                    .subscribeOn(Schedulers.io())
+                    .doOnSubscribe { t0 = System.nanoTime() }
+                    .blockingFirst()
+
+            Result("Gson", formatNumber.format(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - t0)))
+        }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    private fun moshiBenchmark(): Single<Result> {
+        return Single.fromCallable {
+            var t0 = System.nanoTime()
+            PrepassClient.service(MoshiConverterFactory.create())
+                    .getShop()
+                    .subscribeOn(Schedulers.io())
+                    .doOnSubscribe { t0 = System.nanoTime() }
+                    .blockingFirst()
+
+            Result("Moshi", formatNumber.format(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - t0)))
+        }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    class Result(
+            var name: String,
+            var time: String
+    )
+
+    class ResultAdapter(context: Context) : ArrayAdapter<Result>(context, 0) {
+
+        @SuppressLint("ViewHolder")
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val binding = ItemResultBinding.inflate(LayoutInflater.from(context), parent, false)
+
+            val result = getItem(position)
+            binding.title.text = result.name
+            binding.time.text = "${result.time}ms"
+
+            return binding.root
+        }
     }
 }
--- app/src/main/java/kies/prepass/speedtest/api/PrepassService.kt
+++ app/src/main/java/kies/prepass/speedtest/api/PrepassService.kt
@@ -1,10 +1,10 @@
 package kies.prepass.speedtest.api
 
-import io.reactivex.Single
+import io.reactivex.Flowable
 import kies.prepass.speedtest.models.Shop
 import retrofit2.http.GET
 
 interface PrepassService {
     @GET("/kiesproject/prepass-speedtest/data/shops.json")
-    fun getShop(): Single<List<Shop>>
+    fun getShop(): Flowable<List<Shop>>
 }
